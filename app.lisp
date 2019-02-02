fc8644f654d6c9167a798e308a44bb340e3360b9
fireflower0 <fireflower0@hotmail.co.jp>
2019-02-02 15:21:57 +0900

    #7 Fixed a 03-view-text. Use the package.

--- app.lisp
+++ app.lisp
@@ -1,11 +1,15 @@
 (defpackage :sdl2-game-tutorial/app
   (:use :cl)
   (:import-from :sdl2-game-tutorial/01-view-window)
-  (:import-from :sdl2-game-tutorial/02-view-image))
+  (:import-from :sdl2-game-tutorial/02-view-image)
+  (:import-from :sdl2-game-tutorial/03-view-text))
 (in-package :sdl2-game-tutorial/app)
 
 ;; 01：ウィンドウを表示する
 ; (sdl2-game-tutorial/01-view-window:main)
 
 ;; 02：画像を表示する
-(sdl2-game-tutorial/02-view-image:main)
+;; (sdl2-game-tutorial/02-view-image:main)
+
+;; 03：テキストを表示する
+;; (sdl2-game-tutorial/03-view-text:main)
