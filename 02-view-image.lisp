e10343a826f3c346968949e9f457da8c218b16cf
fireflower0 <fireflower0@hotmail.co.jp>
2019-09-03 16:45:13 +0900

    Fixed a 02-view-image

--- 02-view-image.lisp
+++ 02-view-image.lisp
@@ -8,9 +8,6 @@
   (:export :main))
 (in-package :sdl2-game-tutorial/02-view-image)
 
-;; 外部ファイルをロード
-(load "GameUtility/texture.lisp" :external-format :utf-8)
-
 ;; ウィンドウのサイズ
 (defconstant +screen-width+  640) ; 幅
 (defconstant +screen-height+ 480) ; 高さ
@@ -44,10 +41,17 @@
          (sdl2-image:quit)         ; sdl2-image終了処理
          (sdl2-ttf:quit)))))       ; sdl2-ttf終了処理
 
+(defun create-texture (renderer surface)
+  (sdl2:set-color-key surface :true (sdl2:map-rgb (sdl2:surface-format surface) 0 0 0))
+  (sdl2:create-texture-from-surface renderer surface))
+
 (defun main ()
   (with-window-renderer (window renderer)
     ;; 画像ファイル読み込み、画像情報の取得などを行う
-    (let ((img-tex (tex-load-from-file renderer *image-file-path*)))
+    (let* ((surface (sdl2-image:load-image *image-file-path*))
+           (width (sdl2:surface-width surface))
+           (height (sdl2:surface-height surface))
+           (texture (create-texture renderer surface)))
       ;; イベントループ(この中にキー操作時の動作や各種イベントを記述していく)
       (sdl2:with-event-loop (:method :poll)
         ;; キーが押下されたときの処理
@@ -57,15 +61,17 @@
                       (sdl2:push-event :quit))) ; Escキーが押下された場合、quitイベントをキューに加える
         ;; この中に描画処理など各種イベントを記述していく
         (:idle ()
-               (sdl2:set-render-draw-color renderer 0 0 0 255) ; 作図操作(矩形、線、およびクリア)に使用する色を設定
-               (sdl2:render-clear renderer)                    ; 現在のレンダーターゲットを上記で設定した色で塗りつぶして消去
-
+               ; 作図操作(矩形、線、およびクリア)に使用する色を設定
+               (sdl2:set-render-draw-color renderer 0 0 0 255)
+               ; 現在のレンダーターゲットを上記で設定した色で塗りつぶして消去
+               (sdl2:render-clear renderer)
                ;; レンダリング処理
-               (with-slots (renderer width height texture) img-tex
-                 (let ((x-pos   (- (/ +screen-width+  2) (floor width  2)))          ; テキストの表示位置(X座標)計算
-                       (y-pos   (- (/ +screen-height+ 2) (floor height 2))))         ; テキストの表示位置(Y座標)計算
-                   (tex-render img-tex x-pos y-pos)))
-               
-               (sdl2:render-present renderer))                 ; レンダリングの結果を画面に反映
+               (let ((x-pos (- (/ +screen-width+  2) (floor width  2)))  ; テキストの表示位置(X座標)計算
+                     (y-pos (- (/ +screen-height+ 2) (floor height 2)))) ; テキストの表示位置(Y座標)計算
+                 (sdl2:render-copy renderer
+                                   texture
+                                   :dest-rect (sdl2:make-rect x-pos y-pos width height)))
+               ; レンダリングの結果を画面に反映
+               (sdl2:render-present renderer))
         ;; 終了イベント
         (:quit () t)))))
