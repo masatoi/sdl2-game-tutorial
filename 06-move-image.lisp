776f665979358d8a9aac404a1471e8607916becf
fireflower0 <fireflower0@hotmail.co.jp>
2019-09-04 10:56:07 +0900

    Fixed a 06-move-image

--- 06-move-image.lisp
+++ 06-move-image.lisp
@@ -1,75 +1,48 @@
 ;;; 06：画像を動かす
 
-(defpackage :sdl2-game-tutorial/06-move-image
-  (:use :cl)
-  (:import-from :sdl2)
-  (:import-from :sdl2-image)
-  (:import-from :sdl2-ttf)
-  (:export :main))
-(in-package :sdl2-game-tutorial/06-move-image)
-
-;; 外部ファイルをロード
-(load "GameUtility/texture.lisp" :external-format :utf-8)
-
-;; ウィンドウのサイズ
-(defconstant +screen-width+  640) ; 幅
-(defconstant +screen-height+ 480) ; 高さ
+(defpackage #:sdl2-game-tutorial/06-move-image
+  (:use #:cl
+        #:sdl2-game-tutorial/utils)
+  (:export #:main))
+(in-package #:sdl2-game-tutorial/06-move-image)
 
 ;; 画像ファイルへのパス
 (defparameter *image-file-path* "Material/graphics/picture/cat.png")
 
-;; SDL2ライブラリ初期化＆終了処理
-(defmacro with-window-renderer ((window renderer) &body body)
-  ;; SDLの初期化と終了時の処理をまとめて実行
-  `(sdl2:with-init (:video)
-     ;; ウィンドウ作成処理を実行
-     (sdl2:with-window (,window
-                        :title "SDL2 Tutorial 01" ; タイトル
-                        :w     +screen-width+     ; 幅
-                        :h     +screen-height+    ; 高さ
-                        :flags '(:shown))         ; :shownや:hiddenなどのパラメータを設定できる
-       ;; ウィンドウの2Dレンダリングコンテキストを生成
-       (sdl2:with-renderer (,renderer
-                            ,window
-                            :index -1
-                            ;; レンダリングコンテキストを生成するときに使われるフラグの種類
-                            ;; :software      : ソフトウェア レンダラー
-                            ;; :accelerated   : ハードウェア アクセラレーション
-                            ;; :presentvsync  : 更新周期と同期
-                            ;; :targettexture : テクスチャへのレンダリングに対応
-                            :flags '(:accelerated :presentvsync))
-         (sdl2-image:init '(:png)) ; sdl2-imageを初期化(扱う画像形式はPNG ※他にもJPGとTIFが使える)
-         (sdl2-ttf:init)           ; sdl2-ttfを初期化
-         ,@body
-         (sdl2-image:quit)         ; sdl2-image終了処理
-         (sdl2-ttf:quit)))))       ; sdl2-ttf終了処理
-
 (defun main ()
-  (with-window-renderer (window renderer)
-    (let ((img-tex (tex-load-from-file renderer *image-file-path*))
-          (x-pos 0)    ; 初期X座標
-          (y-pos 0))   ; 初期Y座標
-      ;; イベントループ(この中にキー操作時の動作や各種イベントを記述していく)
-      (sdl2:with-event-loop (:method :poll)
-        ;; キーが押下されたときの処理
-        (:keydown (:keysym keysym)
-                  ;; keysymをスキャンコードの数値(scancode-value)に変換して、キー判定処理(scancode=)を行う
-                  (if (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-                      (sdl2:push-event :quit)  ; Escキーが押下された場合、quitイベントをキューに加える
-                      (with-slots (width height) img-tex
+  (with-window-renderer (window renderer "SDL2 Tutorial 06")
+    (let ((tex nil) (w 0) (h 0)
+          (x-pos 0) (y-pos 0))
+      (flet ((image-texture (renderer img &key (r 0) (g 0) (b 0))
+               (multiple-value-bind (texture width height)
+                   (create-image-texture renderer img :r r :g g :b b)
+                 (setf tex texture w width h height))))
+        (image-texture renderer *image-file-path*)
+        ;; イベントループ(この中にキー操作時の動作や各種イベントを記述していく)
+        (sdl2:with-event-loop (:method :poll)
+          ;; キーが押下されたときの処理
+          (:keydown (:keysym keysym)
+                    ;; keysymをスキャンコードの数値(scancode-value)に変換して、キー判定処理(scancode=)を行う
+                    (if (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+                        (sdl2:push-event :quit)  ; Escキーが押下された場合、quitイベントをキューに加える
                         (case (sdl2:scancode keysym)
                           (:scancode-up    (when (> y-pos 0) (decf y-pos)))
-                          (:scancode-down  (when (< y-pos (- +screen-height+ height)) (incf y-pos)))
+                          (:scancode-down  (when (< y-pos (- *screen-height* h)) (incf y-pos)))
                           (:scancode-left  (when (> x-pos 0) (decf x-pos)))
-                          (:scancode-right (when (< x-pos (- +screen-width+ width)) (incf x-pos)))))))
-        ;; この中に描画処理など各種イベントを記述していく
-        (:idle ()
-               (sdl2:set-render-draw-color renderer 0 0 0 255) ; 作図操作(矩形、線、およびクリア)に使用する色を設定
-               (sdl2:render-clear renderer)                    ; 現在のレンダーターゲットを上記で設定した色で塗りつぶして消去
+                          (:scancode-right (when (< x-pos (- *screen-width* w)) (incf x-pos))))))
+          ;; この中に描画処理など各種イベントを記述していく
+          (:idle ()
+                 ;; 作図操作(矩形、線、およびクリア)に使用する色を設定
+                 (sdl2:set-render-draw-color renderer 0 0 0 255)
+                 ;; 現在のレンダーターゲットを上記で設定した色で塗りつぶして消去
+                 (sdl2:render-clear renderer)
 
-               ;; レンダリング処理
-               (tex-render img-tex x-pos y-pos)
-               
-               (sdl2:render-present renderer))                 ; レンダリングの結果を画面に反映
-        ;; 終了イベント
-        (:quit () t)))))
+                 ;; レンダリング処理
+                 (sdl2:render-copy renderer
+                                   tex
+                                   :dest-rect (sdl2:make-rect x-pos y-pos w h))
+
+                 ;; レンダリングの結果を画面に反映
+                 (sdl2:render-present renderer))
+          ;; 終了イベント
+          (:quit () t))))))
